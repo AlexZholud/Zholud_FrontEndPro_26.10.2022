7904c0849335d970903e20b0244701276b9b0e80
AlexZholud <24slim07@gmail.com>
2022-10-28 23:33:58 +0300

    hours to seconds homework

---
+++ hoursToSeconds_hw/hoursToSeconds/index.js
@@ -0,0 +1,7 @@
+let numberOfHours = prompt(`Please enter number of hours:`);
+
+let numberOfMinutes = (numberOfHours * 60);
+let numberOfSeconds = (numberOfMinutes * 60);
+
+
+alert(`Your result:\nYou have ${numberOfSeconds} seconds in ${numberOfHours} hour(s)!`);
